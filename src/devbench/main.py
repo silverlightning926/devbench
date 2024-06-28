cdd135765ff8f5a4453d5888534e3f7e9badfb0a
Siddharth Rao <siddrao.926@gmail.com>
2024-06-28 00:24:06 -0700

    Added Initial Doctor Command

--- src/devbench/main.py
+++ src/devbench/main.py
@@ -1,6 +1,12 @@
 import typer
 from typing import Optional
 import importlib.metadata
+from rich.progress import Progress
+from rich.console import Console
+from rich.table import Table
+from rich import print
+import subprocess
+import os
 
 __version__ = importlib.metadata.version('devbench')
 
@@ -9,7 +15,80 @@ app = typer.Typer(name="devbench", add_completion=False)
 
 @app.command()
 def doctor():
-    typer.echo("Doctor Command")
+    print("[bold purple]DevBench Doctor[/bold purple]")
+    print(f"[purple]DevBench Version v{__version__}[/purple]\n")
+
+    compilation_environments = {
+        "C": ["gcc", "--version"],
+        "C++": ["g++", "--version"],
+        "C#": ["dotnet", "--info"],
+        "Java": ["javac", "-version"],
+        "Kotlin": ["kotlin", "-version"],
+        "Rust": ["rustc", "--version"],
+        "Go": ["go", "version"],
+    }
+
+    runtime_environments = {
+        "C#": ["dotnet", "--version"],
+        "Java": ["java", "-version"],
+        "Kotlin": ["kotlin", "-version"],
+        "Go": ["go", "version"],
+        "Node.js": ["node", "--version"],
+        "Python": ["python", "--version"],
+        "Ruby": ["ruby", "--version"],
+    }
+
+    tool_environments = {
+        "git": ["git", "--version"],
+        "pip": ["pip", "--version"],
+        "npm": ["npm", "--version"],
+        "gem": ["gem", "--version"],
+        "cargo": ["cargo", "--version"],
+        "Gradle": ["Gradle", "--version"],
+        "Docker": ["docker", "--version"],
+    }
+
+    console = Console()
+
+    compilation_table = Table(
+        title="Compilation Environments", title_justify="left")
+    compilation_table.add_column("Environment")
+    compilation_table.add_column("Status")
+    check_environments(compilation_environments,
+                       compilation_table, console)
+
+    runtime_table = Table(title="\nRuntime Environments", title_justify="left")
+    runtime_table.add_column("Environment")
+    runtime_table.add_column("Status")
+    check_environments(runtime_environments, runtime_table, console)
+
+    runtime_table = Table(title="\nTool Environments", title_justify="left")
+    runtime_table.add_column("Environment")
+    runtime_table.add_column("Status")
+    check_environments(tool_environments, runtime_table, console)
+
+
+def check_environments(environments, table, console):
+    with Progress(transient=True) as progress:
+        task = progress.add_task(
+            "[green]Checking...", total=len(environments))
+
+        for env, cmd in environments.items():
+            progress.update(
+                task, description=f"[green]Checking {env}...")
+
+            try:
+                subprocess.run(
+                    cmd, env=os.environ, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+                status = "✅"
+            except FileNotFoundError:
+                status = "❌"
+
+            table.add_row(env, status)
+            progress.update(task, advance=1)
+
+    console.print(table)
 
 
 @app.command()
